57517aac07aaae1b4366335abd66096257107d0b
Karan2310 <karandeep9619@gmail.com>
2023-12-04 12:12:25 +0530

    Config

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-// export const SERVER_URL = "https://squad-link-api.vercel.app";
-export const SERVER_URL = "http://192.168.0.110:8001";
+export const SERVER_URL = "https://squad-link-api.vercel.app";
+// export const SERVER_URL = "http://192.168.0.110:8001";
